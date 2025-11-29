8b20af7f6e7766245553ac9d34e90903c7e6c3e3
Isiah61706 <isiah617@icloud.com>
2025-11-29 03:46:45 +0000

    fibonacci function

--- my_math.py
+++ my_math.py
@@ -61,3 +61,15 @@ def isPrime(x):
 
 def isEven(x):
     return x % 2 ==0
+
+def fibonacci(n):
+    if n < 0:
+        raise ValueError("Fibonacci not defined for negative numbers")
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
